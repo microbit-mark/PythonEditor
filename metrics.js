6c083b06f5a2dbd15fdf32f4cdf720b7088674be
Daniel Baines <49149587+danbai02@users.noreply.github.com>
2019-06-19 14:39:46 +0100

    Update metrics.js to track script size in lines (#51)

--- metrics.js
+++ metrics.js
@@ -1,14 +1,37 @@
 function sendMetric(slug) {
-  $.ajax({
-    type: "GET",
-    url: "https://metrics.microbit.org/pyeditor-" + EDITOR_VERSION + slug,
-    complete: function(res) {
-    }
-  })
+    $.ajax({
+      type: "GET",
+      url: "https://metrics.microbit.org/pyeditor-" + EDITOR_VERSION + slug,
+      complete: function(res) {
+      }
+    })
+}
+
+function trackLines() {
+    var range = [[0, 20], [21, 30], [31, 50], [51, 100], [101, 200], [201, 1000], [1001, 1000000]];
+    var lines = EDITOR.getCode().split(/\r\n|\r|\n/).length;
+
+    var bucket = range.filter(function(a) {
+      if (lines >= a[0] && lines <= a[1]) return a;
+    })
+
+    var slug = "/lines/" + bucket[0].toString();
+    sendMetric(slug);
 }
 
 $(".action").click(function (e) {
     var slug = "/action/" + $(e.target).closest(".action")[0].id;
     slug = slug.replace("command-", "");
-    sendMetric(slug);
+
+    switch(slug) {
+      case "/action/download":
+      case "/action/flash":
+      case "/action/save":
+      case "/action/save-hex":
+        sendMetric(slug);
+        trackLines();
+        break;
+      default:
+        sendMetric(slug);
+    }
 });
