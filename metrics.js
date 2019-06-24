b6bae4fe4e57d01df24a7f034c3ce1de49a3e48c
Daniel Baines <49149587+danbai02@users.noreply.github.com>
2019-06-24 12:03:32 +0100

    Updated metrics.js to track number of files/page loads/dropped loading (#52)

--- metrics.js
+++ metrics.js
@@ -19,10 +19,77 @@ function trackLines() {
     sendMetric(slug);
 }
 
+function trackFiles() {
+    var range = [[11, 15], [16, 20], [21, 25], [26, 1000]];
+    var files;
+
+    try {
+      // Will always be at least 1 due to main.py
+      files = micropythonFs.ls().length;
+    }
+    catch {
+      // If the filesystem is not present
+      sendMetric("/files/-1");
+      return;
+    }
+
+    if (files > 10) {
+      var bucket = range.filter(function(a) {
+        if (files >= a[0] && files <= a[1]) return a;
+      })
+
+      var slug = "/files/" + bucket[0].toString();
+      sendMetric(slug);
+    }
+    else {
+      var slug = "/files/" + files.toString();
+      sendMetric(slug);
+    }
+}
+
+window.onload = function() {
+    sendMetric("/page-load/");
+};
+
+// Dropping into editor
+$('#editor').on('drop', function (e) {
+    var file = e.originalEvent.dataTransfer.files[0];
+    var ext = (/[.]/.exec(file.name)) ? /[^.]+$/.exec(file.name) : ["none"];
+
+    switch(ext[0]) {
+      case "py":
+        sendMetric("/drop/py");
+        break;
+      case "hex":
+        sendMetric("/drop/hex");
+        break;
+      default:
+        sendMetric("/drop/invalid");
+    }
+});
+
+// Dropping into load area
+document.addEventListener('load-drop', function (e) {
+    var file = e.detail;
+    var ext = (/[.]/.exec(file.name)) ? /[^.]+$/.exec(file.name) : ["none"];
+
+    switch(ext[0]) {
+      case "py":
+        sendMetric("/drop/py");
+        break;
+      case "hex":
+        sendMetric("/drop/hex");
+        break;
+      default:
+        sendMetric("/drop/invalid");
+    }
+});
+
 $(".action").click(function (e) {
     var slug = "/action/" + $(e.target).closest(".action")[0].id;
     slug = slug.replace("command-", "");
 
+    // Note - The save action has been renamed to save-hex in the combined save/load button
     switch(slug) {
       case "/action/download":
       case "/action/flash":
@@ -30,6 +97,7 @@ $(".action").click(function (e) {
       case "/action/save-hex":
         sendMetric(slug);
         trackLines();
+        trackFiles();
         break;
       default:
         sendMetric(slug);
